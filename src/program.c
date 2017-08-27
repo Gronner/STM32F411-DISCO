7721ca7082956a0a97284efa4b28ce40119abb36
Gronner <Gronner@users.noreply.github.com>
2017-08-27 16:49:50 +0200

    Activate Clock to Peripheral Port D, closes #6

--- src/program.c
+++ src/program.c
@@ -1,9 +1,18 @@
 #include "led.h"
 #include "timer.h"
+
+void mcu_init(void)
+{
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+	__asm("dsb");
+}
+
  
 int main(void)
 {
+	mcu_init();	
 	led_init();	
+	led_on(0);
 	timer_init(TIM2);
 	timer_start(TIM2);
 	timer_enable_interrupt(TIM2, 0, 1);
